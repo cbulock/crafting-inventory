e5c763ad143162766f0c4f5ceebdce66c34e2aa7
Cameron Bulock <cameron@bulock.com>
2024-09-16 20:23:35 -0400

    make each project it's own page

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -3,6 +3,9 @@
 import ProjectView from '@/components/ProjectView';
 import withAuth from '@/components/withAuth';
 
+// This page is deprecated, should become just a list view if not selected project
+// or just redirect to the selected project page
+
 const ProjectsPage = () => (
   <main className="m-4 max-w-xl mx-auto">
     <ProjectView />
